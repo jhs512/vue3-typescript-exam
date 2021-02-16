afd65a4ec59d6284368819e0edc67eb0c4b7c8d2
jhs512 <jangka512@gmail.com>
2021-02-16 11:44:31 +0900

    안쓰는 소스코드 제거

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Id labore corporis temporibus modi atque rem doloremque consectetur repudiandae obcaecati! Accusantium quo, adipisci aliquid magnam maiores nulla rerum recusandae ex? Autem?
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
   }
 })
 </script>
 
 <style>
+html, body {
+  margin:0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
